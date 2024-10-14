56f63e8074b241d7f58fd0ddc18636535e43ac29
codestantceasar <c.akas@alustudent.com>
2024-10-14 04:04:38 -0700

    add backup file

---
+++ backup_archives.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define variables
+REMOTE_USER="cf5923497153"
+REMOTE_HOST="cf5923497153.599ec69a.alu-cod.online"
+REMOTE_PATH="/home"
+# Attention!! The three variables above are my intranet_sandbox parameters. You must replace them by yours!
