0e618b84c1c0a68deb5631c31726258cbeca253d
NK-Adeodatus <a.nkundiman@alustudent.com>
2024-10-14 04:12:53 -0700

    add backup script_2

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Define variables
+
 REMOTE_USER="cf5923497153"
 REMOTE_HOST="cf5923497153.599ec69a.alu-cod.online"
 REMOTE_PATH="/home"
-# Attention!! The three variables above are my intranet_sandbox parameters. You must replace them by yours!
+
+
+mkdir ./archived_logs_group1
+mv heart_rate_log.txt_* archived_logs_group1/
+
+scp -r archived_logs_group1 "$REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH"
+
+
+if [[ $? -eq 0 ]]; then
+    echo "Files Backed-up successfully!!"
+    echo "Your backup location: $REMOTE_PATH"
+else
+    echo "Backup failed"
+fi
