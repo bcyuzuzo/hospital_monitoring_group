92b326b444bc8a4e225d9b08238fa4e29ff8c375
Adossi Fred William <f.adossi@alustudent.com>
2024-10-14 11:33:20 +0200

    Archiving script

--- archive_log.sh
+++ archive_log.sh
@@ -2,11 +2,18 @@
 
 # Check if the file 'heart_rate_log.txt' exists
 if [ -f "heart_rate_log.txt" ]; then
-        timestamp=$(date +"%Y%m%d_%H%M%S")
+	
+	# Establishing the timestamp for the file
+	timestamp=$(date +"%Y%m%d_%H%M%S")
+	
+	# Execute the renaming operation
         mv heart_rate_log.txt heart_rate_log.txt_$timestamp
+	
+	# Output message if successfull
         echo "File heart_rate_log.txt archived successfully!"
         echo "Its new name is heart_rate_log.txt_$timestamp!"
 
 else
+	# Output message if not successfull
         echo "File heart_rate_log.txt not found!"
 fi
